019e00c164fb34da5d6bf0afc54c1aca2b317b0f
Kate Sali <kate.gayka.guseva@gmail.com>
2022-12-13 20:41:13 +0300

    Added test_guest_can_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    ADD_TO_BASKET_ALERT = (By.CSS_SELECTOR, "#messages>:nth-child(1) strong")
+    BOOK_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRICE_INFO_ALERT = (By.CSS_SELECTOR, ".alert-info strong")
+    PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import time
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_btn.click()
+        self.solve_quiz_and_get_code()
+        time.sleep(5) 
+        self.should_be_add_to_basket_alert(self.get_book_name())
+        self.should_be_price_info_alert(self.get_price())
+
+    def get_book_name(self):
+        return(self.browser.find_element(*ProductPageLocators.BOOK_NAME))
+
+    def get_price(self):
+        return(self.browser.find_element(*ProductPageLocators.PRICE))
+
+    def should_be_add_to_basket_alert(self, book_name):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_ALERT), "Success alert is not presented"
+        success_alert = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_ALERT)
+        assert book_name.text in success_alert.text, "Book name is not present in success alert"  
+
+    def should_be_price_info_alert(self, price):
+        assert self.is_element_present(*ProductPageLocators.PRICE_INFO_ALERT), "Info alert is not presented"
+        info_alert = self.browser.find_element(*ProductPageLocators.PRICE_INFO_ALERT)
+        assert price.text in info_alert.text, "Price is not present in info alert"
