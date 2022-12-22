dfdbbe00abd49a554a8d944cece645843a322ae7
Kate Sali <kate.gayka.guseva@gmail.com>
2022-12-22 15:29:39 +0300

    added negativ tests

--- pages/locators.py
+++ pages/locators.py
@@ -10,7 +10,8 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
-    ADD_TO_BASKET_ALERT = (By.CSS_SELECTOR, "#messages>:nth-child(1) strong")
+    SUCCESS_MESSAGE_BOOK_NAME = (By.CSS_SELECTOR, "#messages>:nth-child(1) strong")
     BOOK_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     PRICE_INFO_ALERT = (By.CSS_SELECTOR, ".alert-info strong")
     PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>:nth-child(1)")
--- pages/product_page.py
+++ pages/product_page.py
@@ -8,9 +8,7 @@ class ProductPage(BasePage):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_btn.click()
         self.solve_quiz_and_get_code()
-        time.sleep(3) 
-        self.should_be_add_to_basket_alert(self.get_book_name())
-        self.should_be_price_info_alert(self.get_price())
+        time.sleep(10)
 
     def get_book_name(self):
         return(self.browser.find_element(*ProductPageLocators.BOOK_NAME))
@@ -19,11 +17,19 @@ class ProductPage(BasePage):
         return(self.browser.find_element(*ProductPageLocators.PRICE))
 
     def should_be_add_to_basket_alert(self, book_name):
-        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_ALERT), "Success alert is not presented"
-        success_alert = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_ALERT)
-        assert book_name.text == success_alert.text, "Book name is not present in success alert"  
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE_BOOK_NAME), "Success alert is not presented"
+        success_alert_book_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_BOOK_NAME)
+        assert book_name.text == success_alert_book_name.text, "Book name is not present in success alert"  
 
     def should_be_price_info_alert(self, price):
         assert self.is_element_present(*ProductPageLocators.PRICE_INFO_ALERT), "Info alert is not presented"
         info_alert = self.browser.find_element(*ProductPageLocators.PRICE_INFO_ALERT)
         assert price.text == info_alert.text, "Price is not present in info alert"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but it should"
